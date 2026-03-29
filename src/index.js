0805fc783077fabb7d4a4dc30291a82dbb05ef87
cargo-cmyk <carolin.goetz@arbor-insight.com>
2026-03-29 16:35:13 +0200

    Add js documents

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-import "./App.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
